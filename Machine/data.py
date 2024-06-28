4f2e7c661a97f0aef00f2400307e77b093333567
Olamide <Adekoyaolamide643@gmail.com>
2024-06-28 01:49:58 -0400

    add comment

--- Machine/data.py
+++ Machine/data.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import os 
 import numpy as np
 
+
+# Preprocess the data for use 
 def process_mammographic_data(): 
     path_to_file  = os.path.join(os.getcwd(), "Dataset/mammographic/mammographic_masses.data")
     dataset = pd.read_csv(path_to_file,)
